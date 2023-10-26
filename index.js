abac910237f67e8f05871465a362976eeae627d2
suriyasingaravel <suriyasingaravel710.com>
2023-10-26 22:14:45 +0530

    cors middleware

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const {connection} = require("./db");
 const {userRouter} = require("./routes/user.routes");
 const {noteRouter} = require("./routes/note.routes");
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
-
+app.use(cors())
 
 app.get("/", (req,res)=>{
     res.send({"msg": "Hello from Suriya"})
